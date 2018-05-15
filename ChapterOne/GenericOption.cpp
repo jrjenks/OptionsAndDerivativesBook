245d22d23c95b2a53041f3ff74fc5ee0eaa57cb0
J.R. Jenks <jrjenks@agiletek.com>
2018-05-15 13:55:18 -0500

    Pasted code from book: CARLOS OLIVEIRA, Options and Derivatives Programming in C++, 10.1007/978-1-4842-1814-3_1

--- ChapterOne/GenericOption.cpp
+++ ChapterOne/GenericOption.cpp
@@ -1,12 +1,78 @@
-#include "stdafx.h"
+﻿#include "stdafx.h"
+#include <stdio.h>
 #include "GenericOption.h"
 
+using namespace std;
 
-GenericOption::GenericOption()
+GenericOption::GenericOption(double strike, OptionType type, double cost)
+: m_strike(strike),
+  m_type(type),
+  m_cost(cost)
 {
+
 }
 
+GenericOption::GenericOption(const GenericOption &p)
+: m_strike(p.m_strike),
+  m_type(p.m_type),
+  m_cost(p.m_cost)
+{
+
+}
 
 GenericOption::~GenericOption()
 {
 }
+
+//
+// assignment operator
+GenericOption &GenericOption::operator=(const GenericOption &p)
+{
+    if (this != &p)
+    {
+        m_type = p.m_type;
+        m_strike = p.m_strike;
+        m_cost = p.m_cost;
+    }
+    return *this;
+}
+
+//
+// Computes the value of the option at expiration date.
+// Value depends on the type of option (CALL or PUT) and strike.
+//
+double GenericOption::valueAtExpiration(double underlyingAtExpiration)
+{
+    double value = 0.0;
+
+    if (m_type == OptionType_Call)
+    {
+        if (m_strike < underlyingAtExpiration)
+        {
+            value = underlyingAtExpiration - m_strike;
+        }
+    }
+    else  // it is an OptionType_Put
+    {
+        if (m_strike > underlyingAtExpiration)
+        {
+            value = m_strike - underlyingAtExpiration;
+        }
+    }
+    return value;
+}
+
+//
+// return the profit (value at expiration minus option cost)
+//
+double GenericOption::profitAtExpiration(double underlyingAtExpiration)
+{
+    double value = 0.0;
+    double finalValue = valueAtExpiration(underlyingAtExpiration);
+    if (finalValue > m_cost)
+    {
+        value = finalValue - m_cost;
+    }
+    return value;
+}
+
--- ChapterOne/GenericOption.h
+++ ChapterOne/GenericOption.h
@@ -1,8 +1,27 @@
-#pragma once
-class GenericOption
-{
-public:
-	GenericOption();
-	~GenericOption();
+﻿#pragma once
+#include <stdio.h>
+
+//
+// option types based on direction: call or put
+enum OptionType {
+	OptionType_Call,
+	OptionType_Put
 };
 
+//
+// class the represents a generic option
+//
+class GenericOption {
+public:
+	GenericOption(double strike, OptionType type, double cost);
+	GenericOption(const GenericOption &p);
+	~GenericOption();
+	GenericOption &operator=(const GenericOption &p);
+
+	double valueAtExpiration(double underlyingAtExpiration);
+	double profitAtExpiration(double underlyingAtExpiration);
+private:
+	double m_strike;
+	OptionType m_type;
+	double m_cost;
+};
