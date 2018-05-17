b2d93ddc1f339adaff37176862a319f01cbea6a9
J.R. Jenks <jrjenks@agiletek.com>
2018-05-17 14:40:39 -0500

    Renamed Chapters.

---
+++ Chapter9/PolynomialFunction.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <vector>
+#include "MathFunction.h"
+
+//
+//  Polynomial has the form  c_1 x^n + c_2 x^n-1 + ....  + c_n-1 x^1 + c_n
+//
+class PolynomialFunction : public MathFunction {
+public:
+	PolynomialFunction(const std::vector<double> &coef);
+	PolynomialFunction(const PolynomialFunction &p);
+	virtual ~PolynomialFunction();
+	virtual PolynomialFunction &operator=(const PolynomialFunction &p);
+
+	virtual double operator()(double x) override;
+private:
+	std::vector<double> m_coefficients;
+};
